6d3772726aae1a7382e0ddd5bf151901d942342c
GeorgeBennett <GBennettOfficial@outlook.com>
2025-06-29 15:40:05 -0400

    deploy

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chatty-client-h2p3.onrender.com/"],
+    origin: "https://chatty-client-h2p3.onrender.com",
     credentials: true,
   },
 });
